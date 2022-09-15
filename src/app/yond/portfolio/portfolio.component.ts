b889d97de96a2e4f95017a5ddf79831166b2688b
Fabricio Cortes <fabriciocortes@gmail.com>
2022-09-15 17:34:49 -0300

    adicionando funcionalidade ao select

--- src/app/yond/portfolio/portfolio.component.ts
+++ src/app/yond/portfolio/portfolio.component.ts
@@ -10,6 +10,24 @@ export class PortfolioComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+
+
   }
+
+  onSelectChange (event:any) {
+    const cards = document.querySelectorAll('.example-card');
+   if(cards){
+    cards.forEach((element:any) => {
+      if(element.dataset.type === event.target.value || event.target.value === "default"){
+        element.style.display = "block"
+      }else{
+        element.style.display = "none"
+      }
+
+    });
+
+  }
+   }
+
 
 }
